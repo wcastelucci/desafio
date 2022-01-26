9a49f459386010c14bb125fa1e88eb0f1b71291e
Walter Casteluccci Neto <96139813+wcastelucci@users.noreply.github.com>
2022-01-26 20:22:28 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using cls.pasta;
+
+namespace objeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero Arus = new Hero("Arus", 23, "Knight");
+            wizard wizard = new wizard("wizard", 99, "Devil");
+
+            Console.WriteLine(wizard.attack());
+            Console.WriteLine(Arus.attack());
+        }
+    }
+}
